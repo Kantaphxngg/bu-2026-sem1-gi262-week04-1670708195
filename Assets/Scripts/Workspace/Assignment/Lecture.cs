01f0d4540e23043be303961e658b0928590ba05a
Kantaphxngg <kantaphong.wong@bumail.net>
2026-09-14 21:20:19 +0700

    ืทำไปแล้วนิดหน่อย

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -22,7 +22,28 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            // string[] playerName = new string[1000];
+
+            // ->null
+            LinkedList<string> linkedlist = new LinkedList<string>();
+
+            // [Node 1] ->null
+            linkedlist.AddLast("Node 1");
+            // [Node 1] -> [Node 2] -.null
+            linkedlist.AddLast("Node 2");
+            // [Node 0] -> [Node 1] -> [Node 2] -.null
+            linkedlist.AddFirst("Node 0");
+            
+            LinkedListNode<string> node1 = linkedlist.Find("Node 1");
+            Debug.Log(node1.Value);
+            Debug.Log(node1.Next.Value);
+            Debug.Log(node1.Previous.Value);
+
+
+            foreach (var item in linkedlist)
+            {
+                Debug.Log(item);
+            }
         }
 
         public void LCT03_SyntaxHashTable()
