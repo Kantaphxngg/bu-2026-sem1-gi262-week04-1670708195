d8683bf817c64e642ef08ad64143b03b87b4380a
Kantaphxngg <kantaphong.wong@bumail.net>
2026-09-21 23:56:27 +0700

    ??

--- Assets/Scripts/Workspace/Assignment/Assignment.cs
+++ Assets/Scripts/Workspace/Assignment/Assignment.cs
@@ -1,136 +1,424 @@
-using UnityEngine;
 using System.Collections.Generic;
-
+using Unity.VisualScripting;
+using UnityEngine;
+ 
 namespace Assignment
 {
     public class Assignment : MonoBehaviour
     {
         public void Start()
         {
-            // AS01_CountWords();
-            // AS02_CountNumber();
-            // AS03_CheckValidBrackets();
-            // AS04_PrintReverseLinkedList();
-            // AS05_FindMiddleElement();
-            // AS06_MergeDictionaries();
-            // AS07_RemoveDuplicatesFromLinkedList();
-            // AS08_TopFrequentNumber();
-            // AS09_PlayerInventory();
-            // AS10_GameEventQueue();
-            // AS11_PlayerStatsTracker();
+            //AS01_CountWords();
+            //AS02_CountNumber();
+            //AS03_CheckValidBrackets();
+            //AS04_PrintReverseLinkedList();
+            //AS05_FindMiddleElement();
+            //AS06_MergeDictionaries();
+            //AS07_RemoveDuplicatesFromLinkedList();
+            //AS08_TopFrequentNumber();
+            AS09_PlayerInventory();
+            //AS10_GameEventQueue();
+            //AS11_PlayerStatsTracker();
         }
-
+ 
         #region Assignment
-
+ 
         [Header("AS01 - Count Words")]
         [SerializeField] private string[] as01Words;
-
+ 
         public void AS01_CountWords()
         {
             string[] words = as01Words;
-            throw new System.NotImplementedException();
+ 
+            Dictionary<string, int> wordCounts =new Dictionary<string, int>();
+ 
+            for (int i = 0; i < as01Words.Length; i++)
+            {
+                string inputword = as01Words[i];
+ 
+                if (wordCounts.ContainsKey(inputword))
+                {
+                    wordCounts[inputword]++;
+                }
+                else
+                {
+                    wordCounts.Add(inputword, 1);
+                }
+            }
+            string[] word = new string[wordCounts.Count];
+            int[] counts = new int[wordCounts.Count];
+ 
+            wordCounts.Keys.CopyTo(word, 0);
+            wordCounts.Values.CopyTo(counts, 0);
+ 
+            for (int i = 0; i < word.Length; i++)
+            {
+                Debug.Log("word: " + word[i] + " count: " + counts[i]);
+            }
         }
-
+ 
         [Header("AS02 - Count Number")]
         [SerializeField] private int[] as02Numbers;
-
+ 
         public void AS02_CountNumber()
         {
             int[] numbers = as02Numbers;
-            throw new System.NotImplementedException();
+ 
+            Dictionary<int, int> numberCounts = new Dictionary<int, int>();
+ 
+            for (int i = 0; i < as02Numbers.Length; i++)
+            {
+                int number = as02Numbers[i];
+ 
+                if (numberCounts.ContainsKey(number))
+                {
+                    numberCounts[number]++;
+                }
+                else
+                {
+                    numberCounts.Add(number, 1);
+                }
+            }
+ 
+            int[] num = new int[numberCounts.Count];
+            int[] counts = new int[numberCounts.Count];
+ 
+            numberCounts.Keys.CopyTo(num, 0);
+            numberCounts.Values.CopyTo(counts, 0);
+ 
+            // แสดงผลลัพธ์
+            for (int i = 0; i < num.Length; i++)
+            {
+                Debug.Log("number: " + num[i] + " count: " + counts[i]);
+            }
         }
-
+ 
         [Header("AS03 - Check Valid Brackets")]
         [SerializeField] private string as03Input;
-
+ 
+ 
         public void AS03_CheckValidBrackets()
         {
             string input = as03Input;
-            throw new System.NotImplementedException();
+            Dictionary<char, char> brackets =
+            new Dictionary<char, char>()
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' }
+            };
+ 
+            LinkedList<char> stack = new LinkedList<char>();
+ 
+            bool isValid = true;
+ 
+            for (int i = 0; i < as03Input.Length; i++)
+            {
+                char current = as03Input[i];
+ 
+ 
+                if (brackets.ContainsKey(current))
+                {
+                    stack.AddLast(current);
+                }
+ 
+                else if (brackets.ContainsValue(current))
+                {
+ 
+                    if (stack.Count == 0)
+                    {
+                        isValid = false;
+                        break;
+                    }
+ 
+                    char lastOpen = stack.Last.Value;
+ 
+                    if (brackets[lastOpen] != current)
+                    {
+                        isValid = false;
+                        break;
+                    }
+ 
+                    stack.RemoveLast();
+                }
+ 
+            }
+ 
+            if (stack.Count > 0)
+            {
+                isValid = false;
+            }
+ 
+            if (isValid)
+            {
+                Debug.Log("Valid");
+            }
+            else
+            {
+                Debug.Log("Invalid");
+            }
         }
-
+ 
         [Header("AS04 - Print Reverse Linked List")]
         [SerializeField] private IntLinkedListInput as04List = new IntLinkedListInput();
-
+ 
         public void AS04_PrintReverseLinkedList()
         {
             LinkedList<int> list = as04List.GetLinkedList();
-            throw new System.NotImplementedException();
+ 
+            if (list.Count == 0)
+            {
+                Debug.Log("List is empty");
+                return;
+            }
+ 
+            LinkedListNode<int> current = list.Last;
+ 
+            while (current != null)
+            {
+                Debug.Log(current.Value);
+ 
+                current = current.Previous;
+            }
         }
-
+ 
         [Header("AS05 - Find Middle Element")]
         [SerializeField] private StringLinkedListInput as05List = new StringLinkedListInput();
-
+ 
         public void AS05_FindMiddleElement()
         {
             LinkedList<string> list = as05List.GetLinkedList();
-            throw new System.NotImplementedException();
+            if (list.Count == 0)
+            {
+                Debug.Log("List is empty");
+                return;
+            }
+ 
+            LinkedListNode<string> slow = list.First;
+            LinkedListNode<string> fast = list.First;
+ 
+            while (fast != null && fast.Next != null)
+            {
+                slow = slow.Next;
+ 
+                fast = fast.Next.Next;
+            }
+ 
+            Debug.Log(slow.Value);
         }
-
+ 
         [Header("AS06 - Merge Dictionaries")]
         [SerializeField] private StringIntDictionaryInput as06FirstDictionary = new StringIntDictionaryInput();
         [SerializeField] private StringIntDictionaryInput as06SecondDictionary = new StringIntDictionaryInput();
-
+ 
         public void AS06_MergeDictionaries()
         {
             Dictionary<string, int> dict1 = as06FirstDictionary.GetDictionary();
             Dictionary<string, int> dict2 = as06SecondDictionary.GetDictionary();
-            throw new System.NotImplementedException();
+ 
+            Dictionary<string, int> mergedDictionary = new Dictionary<string, int>(dict1);
+ 
+            foreach (KeyValuePair<string, int> item in dict2)
+            {
+                string key = item.Key;
+                int value = item.Value;
+ 
+                if (mergedDictionary.ContainsKey(key))
+                {
+                    mergedDictionary[key] += value;
+                }
+                else
+                {
+                    mergedDictionary.Add(key, value);
+                }
+            }
+ 
+            foreach (KeyValuePair<string, int> item in mergedDictionary)
+            {
+                Debug.Log("key: " + item.Key + ", value: " + item.Value);
+            }
         }
-
+ 
         [Header("AS07 - Remove Duplicates From Linked List")]
         [SerializeField] private IntLinkedListInput as07List = new IntLinkedListInput();
-
+ 
         public void AS07_RemoveDuplicatesFromLinkedList()
         {
             LinkedList<int> list = as07List.GetLinkedList();
-            throw new System.NotImplementedException();
+ 
+            if (list.Count <= 1)
+            {
+                foreach (int value in list)
+                {
+                    Debug.Log(value);
+                }
+ 
+                return;
+            }
+ 
+            Dictionary<int, bool> seen = new Dictionary<int, bool>();
+ 
+            LinkedListNode<int> current = list.First;
+ 
+            while (current != null)
+            {
+                LinkedListNode<int> next = current.Next;
+ 
+                if (seen.ContainsKey(current.Value))
+                {
+                    list.Remove(current);
+                }
+                else
+                {
+                    seen.Add(current.Value, true);
+                }
+ 
+                current = next;
+            }
+ 
+            foreach (int value in list)
+            {
+                Debug.Log(value);
+            }
         }
-
+ 
         [Header("AS08 - Top Frequent Number")]
         [SerializeField] private int[] as08Numbers;
-
+ 
         public void AS08_TopFrequentNumber()
         {
             int[] numbers = as08Numbers;
-            throw new System.NotImplementedException();
+ 
+            if (numbers == null || numbers.Length == 0)
+            {
+                Debug.Log("Input array is empty");
+                return;
+            }
+ 
+            Dictionary<int, int> frequency = new Dictionary<int, int>();
+ 
+            foreach (int number in numbers)
+            {
+                if (frequency.ContainsKey(number))
+                {
+                    frequency[number]++;
+                }
+                else
+                {
+                    frequency.Add(number, 1);
+                }
+            }
+ 
+            int topNumber = numbers[0];
+            int maxCount = frequency[topNumber];
+ 
+            foreach (int number in numbers)
+            {
+                int currentCount = frequency[number];
+ 
+                if (currentCount > maxCount)
+                {
+                    topNumber = number;
+                    maxCount = currentCount;
+                }
+            }
+ 
+            Debug.Log(topNumber + " count: " + maxCount);
         }
-
+ 
         [Header("AS09 - Player Inventory")]
         [SerializeField] private StringIntDictionaryInput as09Inventory = new StringIntDictionaryInput();
         [SerializeField] private string as09ItemName;
         [SerializeField] private int as09Quantity;
-
+ 
         public void AS09_PlayerInventory()
         {
             Dictionary<string, int> inventory = as09Inventory.GetDictionary();
             string itemName = as09ItemName;
             int quantity = as09Quantity;
-            throw new System.NotImplementedException();
+            if (inventory.ContainsKey(itemName))
+            {
+                inventory[itemName] += quantity;
+            }
+            else
+            {
+                inventory.Add(itemName, quantity);
+            }
+ 
+            foreach (KeyValuePair<string, int> item in inventory)
+            {
+                Debug.Log(item.Key + ": " + item.Value);
+            }
         }
-
+ 
         [Header("AS10 - Game Event Queue")]
         [SerializeField] private GameEventLinkedListInput as10EventQueue = new GameEventLinkedListInput();
-
+ 
         public void AS10_GameEventQueue()
         {
             LinkedList<GameEvent> eventQueue = as10EventQueue.GetLinkedList();
-            throw new System.NotImplementedException();
+            if (eventQueue.Count == 0)
+            {
+                Debug.Log("Event queue is empty");
+                return;
+            }
+ 
+            while (eventQueue.Count > 0)
+            {
+                GameEvent currentEvent = eventQueue.First.Value;
+ 
+                eventQueue.RemoveFirst();
+ 
+                Debug.Log("Processing event: " + currentEvent.Name);
+ 
+                Debug.Log("Remaining events in queue: " + eventQueue.Count);
+ 
+                if (currentEvent.EventType == "enemy")
+                {
+                    Debug.Log("Enemy event processed - " + currentEvent.Name);
+                }
+                else if (currentEvent.EventType == "powerup")
+                {
+                    Debug.Log("Power-up event processed - " + currentEvent.Name);
+                }
+                else if (currentEvent.EventType == "level")
+                {
+                    Debug.Log("Level event processed - " + currentEvent.Name);
+                }
+            }
         }
-
+ 
         [Header("AS11 - Player Stats Tracker")]
         [SerializeField] private StringIntDictionaryInput as11PlayerStats = new StringIntDictionaryInput();
         [SerializeField] private string as11StatName;
         [SerializeField] private int as11Value;
-
+ 
         public void AS11_PlayerStatsTracker()
         {
             Dictionary<string, int> playerStats = as11PlayerStats.GetDictionary();
             string statName = as11StatName;
             int value = as11Value;
-            throw new System.NotImplementedException();
+            if (playerStats.ContainsKey(statName))
+            {
+                playerStats[statName] += value;
+            }
+            else
+            {
+                playerStats.Add(statName, value);
+            }
+ 
+            Debug.Log(
+                "Updated " + statName + ": " + playerStats[statName]
+            );
+ 
+            Debug.Log("Current player statistics:");
+ 
+            foreach (KeyValuePair<string, int> stat in playerStats)
+            {
+                Debug.Log(stat.Key + ": " + stat.Value);
+            }
         }
-
+ 
         #endregion
     }
 }
--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -27,19 +27,37 @@ namespace Assignment
             // ->null
             LinkedList<string> linkedlist = new LinkedList<string>();
 
-            // [Node 1] ->null
+            // [Node 1] -> null
             linkedlist.AddLast("Node 1");
-            // [Node 1] -> [Node 2] -.null
+            // [Node 1] -> [Node 2] -> null
             linkedlist.AddLast("Node 2");
-            // [Node 0] -> [Node 1] -> [Node 2] -.null
+            // [Node 0] -> [Node 1] -> [Node 2] -> null
             linkedlist.AddFirst("Node 0");
             
             LinkedListNode<string> node1 = linkedlist.Find("Node 1");
             Debug.Log(node1.Value);
             Debug.Log(node1.Next.Value);
+            // Debug.Log(node1.Next.Next.Value);
             Debug.Log(node1.Previous.Value);
 
+            var firstNode = linkedlist.First;
+            var lastNode = linkedlist.Last;
+            Debug.Log(firstNode.Previous);
+            Debug.Log(lastNode.Next);
 
+            // [Node 0] -> [Node 1] -> [Node 1.5] -> [Node 2] -> null
+            linkedlist.AddAfter(node1, "Node 1.5");
+            // [Node 0] -> [Node 0.5] -> [Node 1] -> [Node 1.5] -> [Node 2] -> null
+            linkedlist.AddBefore(node1, "Node 0.5");
+
+            // [Node 0.5] -> [Node 1] -> [Node 1.5] -> [Node 2] -> null
+            linkedlist.RemoveFirst();
+            // [Node 0.5] -> [Node 1] -> [Node 1.5] -> null
+            linkedlist.RemoveLast();
+            // [Node 0.5] -> [Node 1] -> null
+            linkedlist.Remove("Node 1.5");
+
+            Debug.Log("~~~~");
             foreach (var item in linkedlist)
             {
                 Debug.Log(item);
@@ -53,8 +71,48 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
-        }
+           Dictionary<string, int> inv = new Dictionary<string, int>();
+           //var inv = new Dictionary<string, int>();
+ 
+           inv.Add("Potion", 5);
+           inv.Add("Banana", 1);
+           inv.Add("Apple", 10);
+ 
+           // Potion: 5
+           // Banana: 1
+           // Apple: 10
+ 
+           // Potion: 5
+           // Banana: 1
+           // Apple: 0
+           inv["Apple"] = 0;
+ 
+           // Potion: 5
+           // Banana: 1
+           // Apple: 0
+           // Apple1: 1
+           inv["Apple1"] = 1;
+ 
+           int potion = inv["Potion"];
+           Debug.Log("potion: " + potion);
+ 
+           //int apple2 = inv["Apple2"];
+           //Debug.Log("apple2: " + apple2);
+ 
+           bool hasPotion = inv.ContainsKey("Potion");
+           Debug.Log("hasPotion: " + hasPotion);
+ 
+           inv.Remove("Banana");
+ 
+           foreach (KeyValuePair<string, int> kvp in inv)
+           {
+               var key = kvp.Key;
+               var value = kvp.Value;
+               Debug.Log($"{key} => {value}");
+               //inv.Add("mm", 1);
+           }
+ 
+           inv.Clear();        }
 
         #endregion
     }
